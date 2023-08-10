0b608c4982d272805d3a92e6fe9a96a71be81e95
Gabriel S Padin <88696395+gabrielspadin22@users.noreply.github.com>
2023-08-10 19:51:52 -0300

    Add files via upload

--- src/Components/Bar/Bar.jsx
+++ src/Components/Bar/Bar.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import './bar.css';
-import { onSnapshot} from 'firebase/firestore';
+import { doc, writeBatch, onSnapshot} from 'firebase/firestore';
 import { useEffect, useState } from "react";
-import { reservasCollectionRef, configCollectionRef } from '../../config/firebase';
-
-
-
-
-
-
+import { reservasCollectionRef, configCollectionRef, db } from '../../config/firebase';
 
 function Bar() {
-    const [listaReservas, setListaReservas] = useState([]);
+    const [listaReservas, setListaReservas] = useState();
     const [cuposDisponibles, setCuposDisponibles] = useState(['']);
     const [time, setTime] = useState(8);
     const select = document.getElementById("horarioBar");
-
+    let nuevosCupos;
     function guardarHorario() {
         const horarioSeleccionado = select.value;
         console.log("Horario seleccionado:", horarioSeleccionado);
         setTime(parseInt(horarioSeleccionado));        
     };
+    
+    let f = new Date();
+
+    function mostrarHora() {
+        let fecha = new Date();
+        let horas = fecha.getHours();
+        let minutos = fecha.getMinutes();
+        
+        horas = (horas < 10) ? '0' + horas : horas;
+        minutos = (minutos < 10) ? '0' + minutos : minutos;
+        
+        let horaActual = horas + ':' + minutos;
+        document.getElementById('reloj').innerHTML = horaActual;
+    }
+    setInterval(mostrarHora, 1000);
 
 
     useEffect(() => {
@@ -45,29 +54,59 @@ function Bar() {
             cupos()
             reservas();
         };
-    }, [time, select]);
+    }, [time, select, nuevosCupos]);
+
+    async function actualizarCupos() {
+        try {
+            const batch = writeBatch(db);
+            const sfRef = doc(db, "config", time.toString());
+            batch.update(sfRef, {cupos: nuevosCupos});
+            await batch.commit();
+        } catch (error) {
+            alert(error)
+        }
+        
+    }
+
 
     return (
-        <div id="tablero" className='bg-dark gap-2'>
-            <div>
-                <label htmlFor="horarioBar">Selecciona un horario:</label>
-                <select id="horarioBar"  onChange={(e) => guardarHorario()}>
-                    <option value={8}>8hs</option>
-                    <option value={9}>9hs</option>
+        <div className='d-flex '>
+            <div className='d-flex flex-column gap-5 bg-secondary bg-opacity-50'>
+                <div>
+                    <h1>RESERVAS DESAYUNO</h1>
+                    <div className='fs-3'>
+                        <div id='dia'>{f.toLocaleDateString()}</div>
+                        <div id="reloj"></div>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="horarioBar">Seleccione un horario:</label>
+                    <select id="horarioBar"  onChange={(e) => guardarHorario()}>
+                        <option value={8}>8hs</option>
+                        <option value={9}>9hs</option>
                     <option value={10}>10hs</option>
-                </select>
+                    </select>
+                    <div className=''>Cupos disponibles: {cuposDisponibles}</div>
+                </div>
+                <div>
+                    <label htmlFor="modificarCupos" className='fs-5'>Modificar cupos:</label>
+                    <input type="number" max={2} id="modificarCupos" onChange={(e) => nuevosCupos = e.target.value} /><br /><br />
+                    <button type='button' onClick={()=>{actualizarCupos(); nuevosCupos = 0}}>Confirmar</button>
+                </div>
+                <div>Añadir horario</div>
             </div>
-            <div className='fs-3 text-light'>Cupos disponibles: {cuposDisponibles}</div>
-            {listaReservas ? listaReservas.map(reserva => 
-            <div key={reserva.id} className='border bg-success p-1 m-1'>
-                <div>Habitacion Nº {reserva.hab}</div>
-                <div>Pax's: {reserva.pax}</div>
-                <div>Horario: {reserva.time}HS</div>
-                <div>Celiaco: {reserva.cel} </div>
-                <div>Vegano: {reserva.veg} </div>
-            </div>) 
-            : 
-            <div>No hay resevas</div>}
+            <div id="tablero" className='bg-dark gap-2'>
+                {listaReservas ? listaReservas.map(reserva => 
+                <div key={reserva.id} className='border bg-success p-1 m-1'>
+                    <div>Room: {reserva.hab}</div>
+                    <div>Pax's: {reserva.pax}</div>
+                    <div>Horario: {reserva.time}HS</div>
+                    <div>Celiacos: {reserva.cel} </div>
+                    <div>Veganos: {reserva.veg} </div>
+                </div>) 
+                : 
+                <div className='text-light mx-auto fs-1'>No hay resevas creadas</div>}
+            </div>
         </div>
     );
 }
--- src/Components/Hab/Reservas.jsx
+++ src/Components/Hab/Reservas.jsx
@@ -8,112 +8,111 @@ import './Reservas.css';
 import { useParams } from 'react-router';
 
 const Formulario = () => {
+    const {num} = useParams();
+    const [pax, setNumeroPersonas] = useState('');
+    const [cel, setNumeroCeliacos] = useState('');
+    const [veg, setNumeroVeganos] = useState('');
+    const [time, setHorarioAsistencia] = useState(0);
+    const [cuposDisponibles, setCuposDisponibles] = useState('');
+    const [cuposTotalesDisponibles, setCuposTotalesDisponibles] = useState();
+    let found;
+    let t = document.getElementById('8hs');
 
-const {num} = useParams();
-console.log(num);
+    useEffect(()=>{
+        const cupos = onSnapshot(configCollectionRef, (snapshot) => {
+            const configInfo = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setCuposTotalesDisponibles(configInfo);
+            setCuposDisponibles(configInfo);
+        });
+        return () => {
+            cupos();
+        };
+    },[time, t]);
 
-const [hab, setNumeroHabitacion] = useState(num);
-const [pax, setNumeroPersonas] = useState('');
-const [cel, setNumeroCeliacos] = useState('');
-const [veg, setNumeroVeganos] = useState('');
-const [time, setHorarioAsistencia] = useState(0);
-const [cuposDisponibles, setCuposDisponibles] = useState('');
-const [cuposTotalesDisponibles, setCuposTotalesDisponibles] = useState();
-
-let found;
-let t = document.getElementById('8hs');
-useEffect(()=>{
-    const cupos = onSnapshot(configCollectionRef, (snapshot) => {
-        const configInfo = snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
-        setCuposTotalesDisponibles(configInfo);
-        setCuposDisponibles(configInfo);
-        console.log(cuposDisponibles);
-        console.log(time);
-    });
-    return () => {
-        cupos();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        found = cuposDisponibles.find(({ id }) => id === parseInt(time));
+        if (found) {
+            parseInt(pax) > found.cupos || found.cupos === 0 ? 
+            Swal.fire({
+                title: '¡UPS! No quedan disponibles',
+                text: 'Por favor seleccione otro horario',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            }) 
+            :
+            crearObjetoEnFirestore(num, pax, cel, veg, time);
+        }else{
+            console.log("Comuniquese con recepcion");
+        };
     };
-},[time, t]);
-
-
-
-const handleSubmit = (e) => {
-    e.preventDefault();
-
-    found = cuposDisponibles.find(({ id }) => id === parseInt(time));
-    console.log(parseInt(pax));
-    console.log(found)
-    if (found) {
-        parseInt(pax) > found.cupos || found.cupos === 0 ? 
-        Swal.fire({
-            title: '¡UPS! No quedan disponibles',
-            text: 'Por favor seleccione otro horario',
-            icon: 'error',
-            confirmButtonText: 'Ok'
-        }) 
-        :
-        crearObjetoEnFirestore(hab, pax, cel, veg, time);
-    }else{
-        console.log("Comuniquese con recepcion");
-    };
-};
     async function crearObjetoEnFirestore(hab, pax, cel, veg, time) {
-    try {
-    const objeto = {
-        hab: hab,
-        pax: pax,
-        cel: cel,
-        veg: veg,
-        time: time
+        try {
+            const objeto = {
+                hab: hab,
+                pax: pax,
+                cel: cel,
+                veg: veg,
+                time: time
+            };
+            await addDoc(collection(db, 'reservas'), objeto);
+            const batch = writeBatch(db);
+            console.log(time)
+            console.log(found.cupos)
+            const sfRef = doc(db, "config", time.toString());
+            batch.update(sfRef, {cupos: found.cupos - pax});
+            await batch.commit();
+            Swal.fire({
+                title: 'Reserva creada con exito',
+                icon: 'success',
+                confirmButtonText: 'Ok'
+            })
+        } catch (error) {
+            console.error('Error al guardar el objeto:', error);
+            Swal.fire({
+                title: 'Error al realizar reserva',
+                text: 'Favor comuniquese con recepcion al 9911 / 9912',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            })
+        }
     };
-    await addDoc(collection(db, 'reservas'), objeto);
-    const batch = writeBatch(db);
-    console.log(time)
-    console.log(found.cupos)
-    const sfRef = doc(db, "config", time.toString());
-    batch.update(sfRef, {cupos: found.cupos - pax});
-    await batch.commit();
-    console.log("reserva creada atr")
-    } catch (error) {
-        console.error('Error al guardar el objeto:', error);
-    }
-};
 
 return (
     <div className='d-flex flex-column bg-secondary bg-opacity-50 h-100 '>
-    <img id='bg' src={bg} alt="background" />
-    <form onSubmit={handleSubmit} className='my-auto'>
-        <h1 className='my-5'>Reserva de desayuno</h1>
-        <h2 className='my-5'>Habitacion Nº {num}</h2>
-        <label htmlFor="numeroPersonas" className='fs-5'>Cantidad de Personas:</label>
-        <input className='px-1' type="number" id="numeroPersonas" value={pax} min={1} max={6} onChange={(e) => setNumeroPersonas(e.target.value)} required /><br /><br />
-        <label htmlFor="numeroCeliacos" className='fs-5'>Cantidad de Celiacos:</label>
-        <input className='px-1' type="number" id="numeroCeliacos" value={cel} min={0} max={6} onChange={(e) => setNumeroCeliacos(e.target.value)} required /><br /><br />
-        <label htmlFor="numeroVeganos" className='fs-5'>Cantidad de Veganos:</label>
-        <input className='px-1' type="number" id="numeroVeganos" value={veg} min={0} max={6} onChange={(e) => setNumeroVeganos(e.target.value)} required /><br /><br />
-        <label htmlFor="horario">Selecciona un horario:</label>
-        <div className='d-flex gap-2 my-5'>
-            <button type='button' id='8hs' className='ms-auto border btn btn-light btn-outline-secondary' value={8} onClick={()=>{setHorarioAsistencia(8)}}>
-                <p className='fs-3'>8Hs</p>
-                <small>({cuposTotalesDisponibles?cuposTotalesDisponibles[1].cupos : ""} Cupos disponibles)</small>
-            </button>
-            <button type="button" className='border btn btn-light btn-outline-secondary' value={9} onClick={()=>{setHorarioAsistencia(9)}}>
-                <p className='fs-3'>9Hs</p>
+        <img id='bg' src={bg} alt="background" />
+        <form onSubmit={handleSubmit} className='my-auto'>
+            <h1 className='my-5'>Reserva de desayuno</h1>
+            <h2 className='my-5'>Habitacion Nº {num}</h2>
+            <label htmlFor="numeroPersonas" className='fs-5'>Cantidad de Personas:</label>
+            <input className='px-1' type="number" id="numeroPersonas" value={pax} min={1} max={6} onChange={(e) => setNumeroPersonas(e.target.value)} required /><br /><br />
+            <label htmlFor="numeroCeliacos" className='fs-5'>Cantidad de Celiacos:</label>
+            <input className='px-1' type="number" id="numeroCeliacos" value={cel} min={0} max={pax == 0 ? 6 : pax} onChange={(e) => setNumeroCeliacos(e.target.value)} required /><br /><br />
+            <label htmlFor="numeroVeganos" className='fs-5'>Cantidad de Veganos:</label>
+            <input className='px-1' type="number" id="numeroVeganos" value={veg} min={0} max={pax == 0 ? 6 : pax} onChange={(e) => setNumeroVeganos(e.target.value)} required /><br /><br />
+            <label htmlFor="horario">Selecciona un horario:</label>
+            <div className='d-flex gap-2 my-5'>
+                <button type='button' id='8hs' className='ms-auto border btn btn-light btn-outline-warning text-dark' value={8} onClick={()=>{setHorarioAsistencia(8)}}>
+                    <p className='fs-3'>8Hs</p>
+                    <small>({cuposTotalesDisponibles?cuposTotalesDisponibles[1].cupos : ""} Cupos disponibles)</small>
+                </button>
+                <button type="button" className='border btn btn-light btn-outline-warning text-dark' value={9} onClick={()=>{setHorarioAsistencia(9)}}>
+                    <p className='fs-3'>9Hs</p>
                 <small>({cuposTotalesDisponibles?cuposTotalesDisponibles[2].cupos : ""} Cupos disponibles)</small>
-            </button>
-            <button type="button" className='me-auto border btn btn-light btn-outline-secondary' value={10} onClick={()=>{setHorarioAsistencia(10)}}>
-                <p className='fs-3'>10Hs</p>
-                <small>({cuposTotalesDisponibles?cuposTotalesDisponibles[0].cupos : ""} Cupos disponibles)</small>
-            </button>
+                </button>
+                <button type="button" className='me-auto border btn btn-light btn-outline-warning text-dark' value={10} onClick={()=>{setHorarioAsistencia(10)}}>
+                    <p className='fs-3'>10Hs</p>
+                    <small>({cuposTotalesDisponibles?cuposTotalesDisponibles[0].cupos : ""} Cupos disponibles)</small>
+                </button>
+            </div>
+            <input className='btn btn-light btn-outline-success rounded fs-4' type="submit" value="Reservar"/>
+        </form>
+        <div className='mx-auto mt-5'>
+            <img  width={"150px"} id='logo' src={logo} alt="Logo_Pueblo_Nativo" />
         </div>
-        <input className='btn btn-light btn-outline-success rounded fs-4' type="submit" value="Reservar"/>
-    </form>
-    <div className='mx-auto mt-5'>
-        <img  width={"150px"} id='logo' src={logo} alt="Logo_Pueblo_Nativo" />
-    </div>
     </div>
 );};
 
